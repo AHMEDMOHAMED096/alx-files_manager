ce2b76c9d3848397acfd8b9cce0b095b75a09d3a
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-12-11 21:23:41 +0200

    UsersController.js

---
+++ controllers/UsersController.js
@@ -0,0 +1,31 @@
+const crypto = require('crypto');
+const dbClient = require('../utils/db');
+
+class UsersController {
+    static async postNew(req, res) {
+        const { email, password } = req.body;
+
+        if (!email) {
+            return res.status(400).json({ error: 'Missing email' });
+        }
+
+        if (!password) {
+            return res.status(400).json({ error: 'Missing password' });
+        }
+
+        const existingUser = await dbClient.db.collection('users').findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ error: 'Already exist' });
+        }
+
+        const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+        const newUser = { email, password: hashedPassword };
+
+        const result = await dbClient.db.collection('users').insertOne(newUser);
+        const user = { id: result.insertedId, email };
+
+        return res.status(201).json(user);
+    }
+}
+
+module.exports = UsersController;
